4aac6a775442ef176c175b9f3840e706fa724aba
Saurabh Dhumane <saurabhdhumneraje@gmail.com>
2022-08-15 18:59:55 +0530

    all files uploaded

---
+++ Js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    alert("this is only fontend website if you can contact me please mail me 🤗🤗");
+  }
+
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "saurabhdhumneraje@gmail.com or +91 8888211972";
+  }
+
